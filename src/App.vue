bdb7f1fffc2e3f70333df4fe83f6f7b4375f6dba
MartinShelley <martin.shelley91@gmail.com>
2023-04-22 17:01:25 +0100

    adding menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view/>
+  <MenuBar />
+  <!-- <NoInvoices /> -->
 </template>
+
+<script>
+// import NoInvoices from './components/NoInvoices.vue';
+import MenuBar from './components/UI/MenuBar.vue';
+export default {
+  components: {
+    MenuBar,
+    // NoInvoices
+  }
+}
+</script>
 
 <style lang="scss">
 
---
+++ src/components/UI/MenuBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <section id="menu-bar">
+    <div>
+      <img src="@/assets/logo.svg" alt="Invoice App Logo" />
+    </div>
+    <div class="menu-items">
+      <div class="light-mode" @click="toggleColourMode">
+        <img src="@/assets/icon-moon.svg" alt="Dark Mode" v-if="this.lightMode"/>
+        <img src="@/assets/icon-sun.svg" alt="Light Mode" v-else/>
+      </div>
+      <div class="account-icon">
+        <img src="@/assets/account-icon.svg" alt="Account Icon" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        lightMode: true
+      }
+    },
+    methods: {
+      toggleColourMode() {
+        this.lightMode = !this.lightMode;
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  #menu-bar {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 103px;  
+    background-color: #1E2139;  
+    border-radius: 0 20px 20px 0;
+
+    .menu-items {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding-bottom: 24px;
+      gap: 24px;
+
+      .light-mode {
+        border-bottom: 1px solid #494E6E;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding-bottom: 24px;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
